d75bff994bf2144d014a8d9d2f58cf6482c04533
Bao Anh <nguyenlebaoanh05@gmail.com>
2024-11-16 21:02:44 -0500

    path for create task, view task assigned to, and view task created by are added

--- frontend/TaskManager/authentication/urls.py
+++ frontend/TaskManager/authentication/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
+    path('home/', views.home_view, name='home'),
+    path('tasks/create/', views.create_task_view, name='create_task'),
+    path('tasks/assigned/', views.view_task_assigned_view, name='view_task_assigned'),
+    path('tasks/created/', views.view_task_created_view, name='view_task_created'),
 ]
